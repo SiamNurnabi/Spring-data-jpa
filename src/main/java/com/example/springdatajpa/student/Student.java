e916e228455afdbf5b236212d528c143ca89216b
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-05-31 11:58:41 +0600

    Student class is added

---
+++ src/main/java/com/example/springdatajpa/student/Student.java
@@ -0,0 +1,72 @@
+package com.example.springdatajpa.student;
+
+public class Student {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String age;
+
+    public Student(long id,
+                   String firstName,
+                   String lastName,
+                   String email,
+                   String age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
