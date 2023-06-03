ee7b80c8681b07a363920f68c198f052c58a10d9
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-06-03 16:09:46 +0600

    Constructor is modified where id is removed

--- src/main/java/com/example/springdatajpa/student/Student.java
+++ src/main/java/com/example/springdatajpa/student/Student.java
@@ -55,12 +55,10 @@ public class Student {
     )
     private Integer age;
 
-    public Student(long id,
-                   String firstName,
+    public Student(String firstName,
                    String lastName,
                    String email,
                    Integer age) {
-        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
