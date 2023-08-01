caab24e78a7f1d2af4c462b1bc877a366743b25b
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-08-01 12:36:28 +0600

    Two custom query method have added

--- src/main/java/com/example/springdatajpa/student/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/student/StudentRepository.java
@@ -2,6 +2,11 @@ package com.example.springdatajpa.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findStudentByEmail(String email);
+
+    List<Student> findStudentByFirstNameEqualsAndAgeEquals( String firstName,Integer age);
 }
