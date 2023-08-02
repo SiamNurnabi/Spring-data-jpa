dced7f6cd1698cca5a47b070fbd4858d90c3b1e8
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-08-02 11:41:56 +0600

    Faker dependency is added

--- src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
@@ -20,62 +20,7 @@ public class SpringDataJpaApplication {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Student student1 = new Student(
-                    "jarin",
-                    "anjum",
-                    "jarinanjum@gmail.com",
-                    22);
-            Student student2 = new Student(
-                    "sifat",
-                    "nur",
-                    "sifatnur@gmail.com",
-                    20);
-            Student student3 = new Student(
-                    "siam",
-                    "nurnabi",
-                    "siamnurnabi@gmail.com",
-                    27);
-            List studentList = new ArrayList();
-            studentList.add(student1);
-            studentList.add(student2);
-            studentList.add(student3);
-            studentRepository.saveAll(studentList);
-            System.out.println("Student count::::::::::" + studentRepository.count());
 
-            studentRepository
-                    .findStudentByEmail("sifatnur@gmail.com")
-                    .ifPresentOrElse(student -> {
-                        System.out.println(student);
-                    }, () -> {
-                        System.out.println("Student with email sifatnur@gmail.com not found");
-                    });
-            System.out.println("without jpql::::::");
-            studentRepository
-                    .findStudentByFirstNameEqualsAndAgeEquals("jarin", 22)
-                    .forEach(System.out::println);
-
-            System.out.println("with jpql::::::");
-            studentRepository
-                    .findStudentByFirstNameEqualsAndAgeEqualsJpql("jarin", 22)
-                    .forEach(System.out::println);
-
-            System.out.println("with native query::::::");
-            studentRepository
-                    .findStudentsByFirstNameLikeOrLastNameNative("si%","anjum")
-                    .forEach(System.out::println);
-
-            System.out.println("without native query::::::");
-            studentRepository
-                    .findStudentsByFirstNameLikeOrLastName("si%","anjum")
-                    .forEach(System.out::println);
-
-            System.out.println("****with native query and named parameter******");
-            studentRepository
-                    .findStudentsByFirstNameLikeOrLastNameNative_NamedParameter("si%","anjum")
-                    .forEach(System.out::println);
-
-            System.out.println("****Deleting siam******");
-            System.out.println(studentRepository.deleteStudentById(3L));
         };
     }
 
