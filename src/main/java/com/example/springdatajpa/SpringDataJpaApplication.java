266b957810bd939fe20029b7b6de17d7f542aede
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-08-02 12:11:22 +0600

    Sorting related code is written

--- src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,20 +22,31 @@ public class SpringDataJpaApplication {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Faker faker = new Faker();
-            for (int i = 0; i < 20; i++) {
-                String firstName = faker.name().firstName();
-                String lastName = faker.name().lastName();
-                String email = String.format("%s%s@yopmail.com", firstName, lastName);
-                Student student = new Student(
-                        firstName,
-                        lastName,
-                        email,
-                        faker.number().numberBetween(15, 45)
-                );
-                studentRepository.save(student);
-            }
+            generateRandomStudents(studentRepository);
+//            Sort sort = Sort.by(Sort.Direction.ASC, "firstName");
+
+            Sort sort = Sort.by( "firstName").descending()
+                    .and(Sort.by("age").ascending());
+
+            studentRepository.findAll(sort)     //this property name should come from entity class variable
+                    .forEach(student -> System.out.println(student.getFirstName()+" "+student.getAge()));
         };
+    }
+
+    private void generateRandomStudents(StudentRepository studentRepository) {
+        Faker faker = new Faker();
+        for (int i = 0; i < 20; i++) {
+            String firstName = faker.name().firstName();
+            String lastName = faker.name().lastName();
+            String email = String.format("%s%s@yopmail.com", firstName, lastName);
+            Student student = new Student(
+                    firstName,
+                    lastName,
+                    email,
+                    faker.number().numberBetween(15, 45)
+            );
+            studentRepository.save(student);
+        }
     }
 
 }
