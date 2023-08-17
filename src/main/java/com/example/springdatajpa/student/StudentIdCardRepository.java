c029a9d0772d9f4854f9095a8b4995137e7bb975
Siam-Nurnabi <siamnurnabi@gmail.com>
2023-08-17 11:59:01 +0600

    Repository for StudentIdCard is added

---
+++ src/main/java/com/example/springdatajpa/student/StudentIdCardRepository.java
@@ -0,0 +1,6 @@
+package com.example.springdatajpa.student;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StudentIdCardRepository extends CrudRepository<StudentIdCard,Long> {
+}
